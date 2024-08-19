5a3668ac6183888add85ef70191fb62ed544869d
Niariasve <138833714+Niariasve@users.noreply.github.com>
2024-08-18 19:04:52 -0500

    Pantalla de inicio sin terminar

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonGrid, IonRow, IonIcon } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { calendar, bookmark } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonIcon, IonButton, IonGrid, IonRow],
 })
 export class Tab1Page {
-  constructor() {}
+  constructor() {
+    addIcons({ calendar, bookmark });
+  }
 }
