6d348e0d4aface25096b21121a404e669edcb38e
Tomagochito <tbolanos@espol.edu.ec>
2024-08-19 14:49:01 -0500

    minuscula error

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,19 +1,19 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { triangle, ellipse, square, home, qrCode, QrCodeOutline, menuOutline } from 'ionicons/icons';
+import { triangle, ellipse, square, home, qrCode, qrCodeOutline, menuOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
   standalone: true,
-  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, qrCode],
+  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
 })
 export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ triangle, ellipse, square, home, qrCode, QrCodeOutline, menuOutline });
+    addIcons({ triangle, ellipse, square, home, qrCode, qrCodeOutline, menuOutline });
   }
 }
